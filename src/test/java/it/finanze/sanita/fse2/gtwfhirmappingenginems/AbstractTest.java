0661a74a1f4905db2dddc4b040a2d557d0e244ac
Giacomo Noya <87712156+gnoya000@users.noreply.github.com>
2022-10-25 15:34:55 +0200

    fix(test): fixed test01

--- src/test/java/it/finanze/sanita/fse2/gtwfhirmappingenginems/AbstractTest.java
+++ src/test/java/it/finanze/sanita/fse2/gtwfhirmappingenginems/AbstractTest.java
@@ -2,6 +2,7 @@ package it.finanze.sanita.fse2.gtwfhirmappingenginems;
 
 import java.io.File;
 
+import org.bson.Document;
 import org.bson.types.Binary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -16,30 +17,64 @@ import lombok.extern.slf4j.Slf4j;
 public abstract class AbstractTest {
 
 	@Autowired
-	private MongoTemplate mongoTemplate;
+	protected MongoTemplate mongoTemplate;
 
 	protected void dropCollections() {
 		mongoTemplate.dropCollection(StructuresRepo.class);
 	}
-	
+
 	protected void saveStructureDefinition(final String completePath, final String rootDefinition) {
 		try {
 			File folder = new File(completePath);
 			File[] listOfFiles = folder.listFiles();
-			
+
 			for (int i = 0; i < listOfFiles.length; i++) {
 				if (listOfFiles[i].isFile()) {
 					String fileName = listOfFiles[i].getName();
-					byte[] content = FileUtility.getFileFromInternalResources(rootDefinition + File.separator + "structure" +File.separator +fileName);
+					byte[] content = FileUtility.getFileFromInternalResources(
+							rootDefinition + File.separator + "structure" + File.separator + fileName);
 					StructureDefinitionDTO defETY = new StructureDefinitionDTO();
 					defETY.setContentFile(new Binary(content));
 					defETY.setFileName(fileName);
 					mongoTemplate.insert(defETY);
 				}
 			}
-		} catch(Exception ex) {
-			log.error("Errore mentre salvi le structure definition : " ,ex);
-			throw new BusinessException("Errore mentre salvi le structure definition : " ,ex);
+		} catch (Exception ex) {
+			log.error("Errore mentre salvi le structure definition : ", ex);
+			throw new BusinessException("Errore mentre salvi le structure definition : ", ex);
 		}
 	}
+
+	protected Document createFakeDefinitionLeftDoc(Binary structurebin) {
+        Document struttura = new Document();
+        struttura.put("name_definition", "structuredefinition-tleft-snap");
+        struttura.put("filename_definition", "structuredefinition-tleft-snap.json");
+        struttura.put("content_definition", structurebin);
+        return struttura;
+    }
+    protected Document createFakeDefinitionRightDoc(Binary structurebin){
+        Document struttura = new Document();
+        struttura.put("name_definition", "structuredefinition-tright-snap");
+        struttura.put("filename_definition", "structuredefinition-tright-snap.json");
+        struttura.put("content_definition", structurebin);
+        return struttura;
+    }
+    protected Document createFakeMapDoc(Binary mapbin){
+        Document mappa = new Document();
+        mappa.put("name_map", "step01");
+        mappa.put("filename_map","step01.map");
+        mappa.put("content_map", mapbin);
+        return mappa;
+    }
+
+    protected Binary loadMapFromPath(String step, String mapname) {
+        return new Binary(FileUtility.getFileFromInternalResources(step
+                + File.separator + "map" + File.separator + mapname));
+    }
+    protected Binary loadStructurefromPath(String step, String structurename) {
+        return new Binary(FileUtility.getFileFromInternalResources(step
+                + File.separator + "structure" + File.separator + structurename));
+    }
+
+
 }
--- src/test/java/it/finanze/sanita/fse2/gtwfhirmappingenginems/Step01Test.java
+++ src/test/java/it/finanze/sanita/fse2/gtwfhirmappingenginems/Step01Test.java
@@ -5,13 +5,18 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.bson.Document;
+import org.bson.types.Binary;
 import org.hl7.fhir.r5.model.StructureMap;
 import org.hl7.fhir.r5.utils.structuremap.StructureMapUtilities;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 import it.finanze.sanita.fse2.gtwfhirmappingenginems.config.Constants;
@@ -28,22 +33,40 @@ class Step01Test extends AbstractTest {
 	@Autowired
 	private StructureDefinitionSRV structureDefinitionSRV;
 
+
+
 	@BeforeEach
-	void setup() {
-		dropCollections();
-		saveStructureDefinition("src\\test\\resources\\step01\\structure" , "step01");
-		structureDefinitionSRV.postConstruct();
-	}
+    void setup() {
+        mongoTemplate.dropCollection("transform");
+    }
      
-	@Test
-	void testStep01() throws Exception {
-		InputStream xmlStep01 = new ByteArrayInputStream(FileUtility.getFileFromInternalResources("step01" + File.separator + "source" + File.separator + "source.xml"));
-		StructureMapUtilities smu5 = new StructureMapUtilities(ContextHelper.getSimpleWorkerContextR5());
-    	StructureMap structuredMap = smu5.parse(new String(FileUtility.getFileFromInternalResources("step01" + File.separator +"map" + File.separator+"step01.map")) , "map");
-		String json01 = Trasformer.transform(xmlStep01, structuredMap);
-		System.out.println(json01);
-		assertNotNull(json01);
-	}
+	 @Test
+	 void testStep01() throws Exception {
+		 Document doc = new Document();
+		 Document struttura1 = createFakeDefinitionLeftDoc(loadStructurefromPath("step01", "structuredefinition-tleft-snap.json"));
+		 Document struttura2 = createFakeDefinitionRightDoc(loadStructurefromPath("step01", "structuredefinition-tright-snap.json"));
+		 Document mappa = createFakeMapDoc(loadMapFromPath("step01", "step01.map"));
+ 
+		 List<Document> structureToSave = new ArrayList<>();
+		 structureToSave.add(struttura1);
+		 structureToSave.add(struttura2);
+		 List<Document> mapsToSave = new ArrayList<>();
+		 mapsToSave.add(mappa);
+		 doc.put("maps", mapsToSave);
+		 doc.put("definitions", structureToSave);
+		 doc.put("version", "1.0");
+		 doc.put("deleted", false);
+		 
+		 mongoTemplate.insert(doc, "transform");
+		 structureDefinitionSRV.postConstruct();
+		 
+		 InputStream xmlStep01 = new ByteArrayInputStream(FileUtility.getFileFromInternalResources("step01" + File.separator + "source" + File.separator + "source.xml"));
+		 StructureMapUtilities smu5 = new StructureMapUtilities(ContextHelper.getSimpleWorkerContextR5());
+		 StructureMap structuredMap = smu5.parse(new String(FileUtility.getFileFromInternalResources("step01" + File.separator + "map" + File.separator + "step01.map")), "map");
+		 String json01 = Trasformer.transform(xmlStep01, structuredMap);
+		 System.out.println(json01);
+		 assertNotNull(json01);
+	 }
 	
  
 }
