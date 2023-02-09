74e82d5a85ceb14f05f82fda75055d07cebca67e
Giuseppe Baittiner <Giuseppe.Baittiner-CIC@ibm.com>
2023-02-09 16:48:24 +0100

    feat(engine): Add refresh process

--- src/main/java/it/finanze/sanita/fse2/gtwfhirmappingenginems/engines/CdaEnginesManager.java
+++ src/main/java/it/finanze/sanita/fse2/gtwfhirmappingenginems/engines/CdaEnginesManager.java
@@ -1,37 +1,55 @@
 package it.finanze.sanita.fse2.gtwfhirmappingenginems.engines;
 
 import it.finanze.sanita.fse2.gtwfhirmappingenginems.engines.base.Engine;
+import it.finanze.sanita.fse2.gtwfhirmappingenginems.engines.base.EngineBuilder;
+import it.finanze.sanita.fse2.gtwfhirmappingenginems.exception.OperationException;
+import it.finanze.sanita.fse2.gtwfhirmappingenginems.exception.engine.EngineBuilderException;
 import it.finanze.sanita.fse2.gtwfhirmappingenginems.exception.engine.EngineException;
 import it.finanze.sanita.fse2.gtwfhirmappingenginems.exception.engine.EngineInitException;
 import it.finanze.sanita.fse2.gtwfhirmappingenginems.repository.IEngineRepo;
+import it.finanze.sanita.fse2.gtwfhirmappingenginems.repository.entity.engine.EngineETY;
+import lombok.extern.slf4j.Slf4j;
 import org.hl7.fhir.r4.model.Bundle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static it.finanze.sanita.fse2.gtwfhirmappingenginems.config.Constants.Logs.*;
 import static it.finanze.sanita.fse2.gtwfhirmappingenginems.config.EngineCFG.ENGINE_EXECUTOR;
 
+@Slf4j
 @Component
 public class CdaEnginesManager {
 
     private final IEngineRepo repository;
+    private final EngineBuilder builder;
     private final ConcurrentHashMap<String, Engine> engines;
     private volatile boolean ready;
 
-    public CdaEnginesManager(@Autowired IEngineRepo repository) {
+    public CdaEnginesManager(
+        @Autowired IEngineRepo repository,
+        @Autowired EngineBuilder builder
+    ) {
         this.repository = repository;
+        this.builder = builder;
         this.engines = new ConcurrentHashMap<>();
         this.ready = false;
     }
 
     @Async(ENGINE_EXECUTOR)
     public void refresh() {
-        // Set flag
+        log.info("Beginning engine refreshing process");
+        // Start process
+        register(lists());
+        // Set flag (start-up only)
         ready = true;
+        log.info("Finishing engine refreshing process");
     }
 
     public Bundle transform(String cda, String engineId, String objectId) throws IOException {
@@ -42,4 +60,58 @@ public class CdaEnginesManager {
         if (uri == null) throw new EngineException(ERR_ENG_ROOT_MAP);
         return obj.getInstance().transformCdaToFhir(cda, uri);
     }
+
+    public ConcurrentHashMap<String, Engine> engines() {
+        return engines;
+    }
+
+    private List<EngineETY> lists() {
+        List<EngineETY> list = new ArrayList<>();
+        try {
+            // Retrieve available engines
+            list = repository.find();
+        } catch (OperationException e) {
+            log.error("Unable to retrieve all available engines", e);
+        }
+        return list;
+    }
+    private void register(List<EngineETY> list) {
+        for (EngineETY e : list) {
+            String id = e.getId();
+            // Check if instance exists
+            if(!engines.containsKey(id)) {
+                // Spawn engine
+                Optional<Engine> engine = create(id);
+                // Consistency check
+                if(engine.isPresent()) {
+                    // Update
+                    engines.put(id, engine.get());
+                    // Mark as available, if unsuccessful unload from memory
+                    if (!available(id)) engines.remove(id);
+                }
+            } else {
+                log.debug("Skipping engine {} because it already exists in-memory", id);
+            }
+        }
+    }
+    private Optional<Engine> create(String id) {
+        Engine e = null;
+        try {
+            e = builder.fromId(id);
+        } catch (OperationException | EngineBuilderException ex) {
+            log.error("Unable to instance engine", ex);
+            log.error("Skipping {} ...", id);
+        }
+        return Optional.ofNullable(e);
+    }
+    private boolean available(String id) {
+        boolean b = false;
+        try {
+            b = repository.enable(id);
+        } catch (OperationException e) {
+            log.error("Unable to mark engine '{}' as enabled", id);
+        }
+        return b;
+    }
+
 }
