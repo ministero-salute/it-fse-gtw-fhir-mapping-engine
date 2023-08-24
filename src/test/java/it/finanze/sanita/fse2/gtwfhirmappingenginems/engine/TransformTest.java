bf08a2d668dcbff3e7abde6c781c7037f6a0137e
Simone Le Noci <72795995+SimoneNuts@users.noreply.github.com>
2023-08-24 14:39:25 +0200

    feat: TransformTest json

---
+++ src/test/java/it/finanze/sanita/fse2/gtwfhirmappingenginems/engine/TransformTest.java
@@ -0,0 +1,73 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ * 
+ * Copyright 2023 Ministero della Salute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package it.finanze.sanita.fse2.gtwfhirmappingenginems.engine;
+
+import static it.finanze.sanita.fse2.gtwfhirmappingenginems.base.CDA.LAB;
+import static it.finanze.sanita.fse2.gtwfhirmappingenginems.base.Engine.LAB_ENGINE;
+import static it.finanze.sanita.fse2.gtwfhirmappingenginems.config.Constants.Profile.TEST;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.hl7.fhir.exceptions.FHIRException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import it.finanze.sanita.fse2.gtwfhirmappingenginems.engine.base.AbstractEngineTest;
+import it.finanze.sanita.fse2.gtwfhirmappingenginems.service.ITransformerSRV;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ActiveProfiles(TEST)
+@TestInstance(PER_CLASS)
+class TransformTest extends AbstractEngineTest {
+
+	@Autowired
+	private ITransformerSRV service;
+
+	@BeforeEach
+    void onEachSetup() throws IOException {
+        // Restore database
+        initDb();
+        // Reload engines (if required)
+        initEngine();
+    }
+	
+	@Test
+	void transform() throws FHIRException, IOException {
+		String json = service.transform(
+			LAB.read(),
+            LAB_ENGINE.engineId(),
+            LAB_ENGINE.transformId(),
+            null);
+		
+		assertNotNull(json);
+		
+		String filePath = "src/test/resources/transform-test.json"; 
+	    try (FileWriter fileWriter = new FileWriter(filePath)) {
+			fileWriter.write(json);
+		}
+	}
+	
+}
