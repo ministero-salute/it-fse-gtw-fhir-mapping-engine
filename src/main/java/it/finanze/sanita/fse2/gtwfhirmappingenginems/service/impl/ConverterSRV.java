85e8ef9c46ee888f72641fa7214db68122bbdbd6
Giuseppe Baittiner <Giuseppe.Baittiner-CIC@ibm.com>
2023-11-10 10:54:09 +0100

    feat: add op type for gtw

--- src/main/java/it/finanze/sanita/fse2/gtwfhirmappingenginems/service/impl/ConverterSRV.java
+++ src/main/java/it/finanze/sanita/fse2/gtwfhirmappingenginems/service/impl/ConverterSRV.java
@@ -8,8 +8,14 @@ import org.bson.Document;
 import org.hl7.fhir.r4.model.*;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static it.finanze.sanita.fse2.gtwfhirmappingenginems.enums.GtwOperationEnum.REPLACE;
 import static it.finanze.sanita.fse2.gtwfhirmappingenginems.utility.FHIRR4Helper.deserializeResource;
 import static it.finanze.sanita.fse2.gtwfhirmappingenginems.utility.FHIRR4Helper.serializeResource;
+import static org.hl7.fhir.r4.model.Bundle.BundleType.DOCUMENT;
 import static org.hl7.fhir.r4.model.Bundle.BundleType.MESSAGE;
 
 @Service
@@ -57,7 +63,40 @@ public class ConverterSRV implements IConverterSRV {
 
     @Override
     public String toDocument(String transaction, GtwOperationEnum op) {
-        return null;
+        // Decode transaction as Bundle
+        Bundle doc = deserializeResource(Bundle.class, transaction, true);
+
+        // Create new Bundle as Document
+        doc.setType(DOCUMENT);
+
+        // Find Composition from bundle
+        Optional<Bundle.BundleEntryComponent> composition = doc.getEntry()
+                .stream()
+                .filter(entry -> ResourceType.Composition.equals(entry.getResource().getResourceType()))
+                .findFirst();
+
+        // Integrity check
+        if (composition.isEmpty()) {
+            throw new IllegalStateException("Cannot transform transaction to document due to missing Composition");
+        }
+
+        // Place composition at the first position of entries
+        doc.getEntry().remove(composition.get());
+        doc.getEntry().add(0, composition.get());
+
+        // Relates the resources
+        for (Bundle.BundleEntryComponent entry : doc.getEntry()) {
+            if(ResourceType.DocumentReference.equals(entry.getResource().getResourceType()) && op.equals(REPLACE)) {
+                DocumentReference docRef = (DocumentReference)entry.getResource();
+                DocumentReference.DocumentReferenceRelatesToComponent relatesTo = new DocumentReference.DocumentReferenceRelatesToComponent();
+                relatesTo.setId(UUID.randomUUID().toString());
+                docRef.setRelatesTo(List.of(relatesTo));
+            }
+            // Remove request from Document type
+            entry.setRequest(null);
+        }
+
+        return serializeResource(doc, true, false, false);
     }
 
 
