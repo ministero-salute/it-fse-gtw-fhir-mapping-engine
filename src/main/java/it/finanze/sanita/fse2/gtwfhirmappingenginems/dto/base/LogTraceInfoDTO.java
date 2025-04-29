6f3831aa8fbe81f191e1f304b058b449f65cfa13
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2025-04-29 18:44:31 +0200

    Update LogTraceInfoDTO.java

--- src/main/java/it/finanze/sanita/fse2/gtwfhirmappingenginems/dto/base/LogTraceInfoDTO.java
+++ src/main/java/it/finanze/sanita/fse2/gtwfhirmappingenginems/dto/base/LogTraceInfoDTO.java
@@ -23,12 +23,7 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class LogTraceInfoDTO extends AbstractDTO {
-
-	/**
-	 * Serial version uid.
-	 */
-	private static final long serialVersionUID = -2079576276445110017L;
-
+ 
 	/**
 	 * Span.
 	 */
