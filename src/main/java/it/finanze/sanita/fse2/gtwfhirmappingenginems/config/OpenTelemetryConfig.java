3ca55f8c184267015bf3982514cdf445c02dcafd
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2025-09-18 16:16:20 +0200

    feat: Update mongo for otel

--- src/main/java/it/finanze/sanita/fse2/gtwfhirmappingenginems/config/OpenTelemetryConfig.java
+++ src/main/java/it/finanze/sanita/fse2/gtwfhirmappingenginems/config/OpenTelemetryConfig.java
@@ -1,16 +1,30 @@
 package it.finanze.sanita.fse2.gtwfhirmappingenginems.config;
 
+import static it.finanze.sanita.fse2.gtwfhirmappingenginems.config.Constants.Properties.MS_NAME;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.trace.Tracer;
-import static it.finanze.sanita.fse2.gtwfhirmappingenginems.config.Constants.Properties.MS_NAME;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+import jakarta.annotation.PostConstruct;
+
 
 @Configuration
 public class OpenTelemetryConfig {
 
-    @Bean
-    public Tracer tracer() {
-        return GlobalOpenTelemetry.getTracer(MS_NAME);
-    }
+	@Autowired
+	private AutoConfiguredOpenTelemetrySdk autoConfiguredOpenTelemetrySdk;
+
+	@PostConstruct
+	public void init() {
+		GlobalOpenTelemetry.set(autoConfiguredOpenTelemetrySdk.getOpenTelemetrySdk());
+	}
+
+	@Bean
+	public Tracer tracer() {
+		return GlobalOpenTelemetry.getTracer(MS_NAME);
+	}
 }
