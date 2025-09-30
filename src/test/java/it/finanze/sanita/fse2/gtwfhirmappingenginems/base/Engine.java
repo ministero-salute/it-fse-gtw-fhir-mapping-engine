5858a418c0af7fa026d38e7936643be55ae8c874
Gianluca Mannini <Gianluca.Mannini-CIC@ibm.com>
2025-09-30 10:56:35 +0200

    feat: add cda transform param test

--- src/test/java/it/finanze/sanita/fse2/gtwfhirmappingenginems/base/Engine.java
+++ src/test/java/it/finanze/sanita/fse2/gtwfhirmappingenginems/base/Engine.java
@@ -19,7 +19,13 @@ package it.finanze.sanita.fse2.gtwfhirmappingenginems.base;
 
 public enum Engine {
 
-    LAB_ENGINE("68777b7f3972d22d71c1135e", "6877657258e2f14cfcb3f9f8"),
+    LAB_ENGINE("6877bb031e71a91de9280aff", "6877bac1c799cf749958624e"),
+    RAD_ENGINE("6877bb031e71a91de9280aff", "6877657b58e2f14cfcb3f9f9"),
+    LDO_ENGINE("6877bb031e71a91de9280aff", "6877655e58e2f14cfcb3f9f6"),
+    VPS_ENGINE("6877bb031e71a91de9280aff", "6877655858e2f14cfcb3f9f5"),
+    RSA_ENGINE("6877bb031e71a91de9280aff", "6877658758e2f14cfcb3f9fa"),
+    PSS_ENGINE("6877bb031e71a91de9280aff", "6877656758e2f14cfcb3f9f7"),
+    PTO_ENGINE("6877bb031e71a91de9280aff", "6877bad4c799cf7499586253"),
     REMOVABLE("6877a5c64afe567ca0ac23aa", "6877657258e2f14cfcb3f9f8"),
     INVALID("INVALID_ENGINE_ID", "INVALID_TRANSFORM_ID");
 
